fdc6ae06c03f3f297018832fdafecbf1ab19c1a1
Anna Nguyen <anna.nguyen42@gmail.com>
2023-04-09 20:21:10 -0400

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+(() => {
+  const wombatFacts = [
+    'Wombats are marsupials native to Australia.',
+    'Wombats have rear-facing pouches, unique among marsupials.',
+    'Wombats prefer to spend most of their time in their burrows.',
+    'Wombats can run up to 25 miles per hour for short distances.',
+    'Baby wombats are called joeys.',
+    'Wombat poop is cube-shaped, which is highly unusual in the animal kingdom.'
+  ];
+
+  const fact = wombatFacts[Math.floor(Math.random()*wombatFacts.length)];
+  document.querySelector('.message').innerHTML = fact;
+})();
